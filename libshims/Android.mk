35dfb7abffcfac40b3390516d2f4dbda195bc203
Aaron Kling <webgeek1234@gmail.com>
2021-12-07 12:38:10 -0600

    flounder: Add protobuf 3.9.1 symlink
    This library is only created on treble targets and is required by
    widevine
    
    Change-Id: I0a07586eb8557d983bcde50a0a3a159dd3367cbb
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,5 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+PROTOBUF_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libprotobuf-cpp-lite-3.9.1.so
+$(PROTOBUF_SYMLINK):
+	$(hide) mkdir -p $(TARGET_OUT_VENDOR)/lib
+	$(hide) ln -sf /system/lib/libprotobuf-cpp-lite.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(PROTOBUF_SYMLINK)
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := camera.cpp
 LOCAL_MULTILIB := 32
